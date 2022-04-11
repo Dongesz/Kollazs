678eace3d901c9444f5698f01072c43acac49d74
Majoros Máté <83972650+Dongesz@users.noreply.github.com>
2022-04-11 18:03:10 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+<script>
+        function osszead() {
+            ossz.value = (Number(x_ert.value) + Number(y_ert.value))
+        }
+
+        function kivon(x, y) {
+            kiv.value = (Number(x_ert.value) - Number(y_ert.value))
+        }
+
+        function szorz(x, y) {
+            szor.value = (Number(x_ert.value) * Number(y_ert.value)).toFixed(2)
+        }
+
+        function oszt(x, y) {
+            osztas.value = (Number(x_ert.value) / Number(y_ert.value)).toFixed(2)
+        }
+    </script>
